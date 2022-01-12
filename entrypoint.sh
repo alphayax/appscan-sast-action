889b5796e96b3af552f772200ec87141fdffae80
alphayax <alphayax@gmail.com>
2022-01-12 15:55:10 +0100

    fix scan filename

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 appscan.sh api_login -P "$APIKEY_SECRET" -u "$APIKEY_ID"
 
 # Generate irx
-appscan.sh prepare -d scan.irx
+appscan.sh prepare -n scan
 
 # Send for Analysis to ASoC
 appscan.sh queue_analysis -a "$APPLICATION_ID" -f scan.irx -n "Scan test github action"
