970c1b954bd41d919f971801b3e83ba8741d0073
alphayax <alphayax@gmail.com>
2022-01-14 16:38:37 +0100

    Change scan message

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ appscan.sh api_login -P "$APIKEY_SECRET" -u "$APIKEY_ID"
 appscan.sh prepare -n scan
 
 # Send for Analysis to ASoC
-appscan.sh queue_analysis -a "$APPLICATION_ID" -f scan.irx -n "Scan test github action"
+appscan.sh queue_analysis -a "$APPLICATION_ID" -f scan.irx -n "Scan github ${GITHUB_SHA}"
